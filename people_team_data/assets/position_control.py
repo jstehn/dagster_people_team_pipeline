61ab0a85320eea2886ce44de8536afa1c9775a8f
Jack Stehn <jack@caliberschools.org>
2025-02-03 17:36:35 +0000

    Allow in ui alterations to configurations

--- people_team_data/assets/position_control.py
+++ people_team_data/assets/position_control.py
@@ -13,7 +13,15 @@ from dagster import (
 )
 from google.oauth2.service_account import Credentials
 
-from ..utils import currency_to_decimal
+from ..config.asset_configs import (
+    TableConfig,
+    pc_adjustments_config,
+    pc_assignments_config,
+    pc_employees_config,
+    pc_positions_config,
+    pc_stipends_config,
+)
+from ..utils import apply_config_to_dataframe
 from ..utils.constants import POSITION_CONTROL_SHEETS_DIR
 
 
@@ -65,7 +73,12 @@ def create_position_control_csv_asset(sheet_name: str, config: dict = None):
     config = config or {}
     deps = ["position_control_sheets"] + [field.split(".")[0] for field in config.get("foreign_keys", {}).values()]
     
-    @asset(name=f"position_control_{sheet_name.lower()}", required_resource_keys={"db_resource"}, deps=[AssetKey(dep) for dep in deps])
+    @asset(
+        name=f"position_control_{sheet_name.lower()}",
+        required_resource_keys={"db_resource"},
+        deps=[AssetKey(dep) for dep in deps],
+        config_schema=TableConfig
+    )
     def position_control_asset(context: AssetExecutionContext, position_control_sheets: list[str]) -> Output[str]:
         """Loads the contents of a specific CSV file for a sheet and uploads it to the DuckDB database."""
         file_path = next((path for path in position_control_sheets if sheet_name in path), None)
@@ -76,22 +89,7 @@ def create_position_control_csv_asset(sheet_name: str, config: dict = None):
 
         # Apply the configuration to the DataFrame
         if config:
-            # Convert columns to the specified types
-            for column, dtype in config.get("columns", {}).items():
-                if dtype == "currency":
-                    df[column] = pd.to_numeric(df[column].apply(currency_to_decimal), errors='coerce', downcast='float')
-                elif dtype == "datetime64[ns]":
-                    df[column] = pd.to_datetime(df[column], errors='coerce')
-                elif dtype == "int":
-                    df[column] = pd.to_numeric(df[column], errors='coerce', downcast='integer')
-                elif dtype == "float":
-                    df[column] = pd.to_numeric(df[column], errors='coerce', downcast='float')
-                else:
-                    df[column] = df[column].astype(dtype, errors='ignore')
-
-            # Drop rows with null values in non-null columns
-            non_null_columns = config.get("non_null", [])
-            df.dropna(subset=non_null_columns, inplace=True)
+            df = apply_config_to_dataframe(df, config)
 
         context.log.info(f"Loaded CSV file '{file_path}' with {df.shape[0]} rows and {df.shape[1]} columns")
         df.dropna(how='all', inplace=True)
@@ -118,146 +116,8 @@ def create_position_control_csv_asset(sheet_name: str, config: dict = None):
     return position_control_asset
 
 # Create assets for each sheet
-positions_config = {
-    "columns": {
-        "Position_ID": "str",
-        "Position_Unique":  "bool",
-        "Assignment_Count": "int",
-        "Position_Start": "datetime64[ns]",
-        "Position_End": "datetime64[ns]",
-        "Position_Status": "str",
-        "Position_HRDepartment": "str",
-        "Position_HRDivision": "str",
-        "Position_HRSubDepartment": "str",
-        "Position_Code": "str",
-        "Position_Count": "str",
-        "Position_Name": "str",
-        "Position_Account": "str",
-        "Position_Goal": "str",
-        "Position_Full": "str",
-        "Position_Credential": "str",
-        "Position_Supervisor": "str",
-        "Position_Represented": "str",
-        "Notes": "str",
-    },
-    "primary_key": "Position ID",
-    "non_null": ["Position_ID"]
-}
-position_control_positions = create_position_control_csv_asset("Positions", config=positions_config)
-
-employees_config = {
-    "columns": {
-        "Employee_ID": "str",
-        "Employee_InitialHireDate": "datetime64[ns]",
-        "Employee_RehireDate": "datetime64[ns]",
-        "Employee_EndDate": "datetime64[ns]",
-        "Employee_Type": "str",
-        "Employee_Status": "str",
-        "Employee_FirstName": "str",
-        "Employee_MiddleName": "str",
-        "Employee_LastName": "str",
-        "Employee_Email": "str",
-        "Employee_Phone": "str",
-        "Employee_YearsOfServiceStart": "str",
-        "Employee_FullName": "str",
-        "Employee_Full": "str",
-        "Employee_SEID": "str",
-        "Employee_Gender": "str",
-        "Employee_Race": "str",
-        "Employee_Hispanic": "str",
-        "Employee_CALPADSStartYearCertificated": "str",
-        "Employee_CALPADSCaliberStartYearCertificated": "str",
-        "Notes": "str",
-    },
-    "primary_key": "Employee_ID",
-    "non_null": ["Employee_ID"]
-}
-position_control_employees = create_position_control_csv_asset("Employees", config=employees_config)
-
-
-adjustments_config = {
-    "columns": {
-        "Adjustment_ID": "str", # Meant to be ID but is currently blank
-        "Assignment_Full": "str",
-        "Adjustment_Status": "str",
-        "Adjustment_Category": "str",
-        "Adjustment_Description": "str",
-        "Adjustment_Salary": "currency",
-        "Adjustment_Hourly": "currency",
-        "Adjustment_Begin_Payroll": "datetime64[ns]",
-        "Adjustment_End_Payroll": "datetime64[ns]",
-        "Adjustment_#": "str",
-        "Adjustment_PPP": "datetime64[ns]",
-        "Notes": "str",
-    },
-    "non_null": ["Assignment_Full", "Adjustment_Status"],
-    "foreign_keys": {
-        "Assignment_Full": "position_control_assignments.Assignment_Full"
-    }
-}
-position_control_adjustments = create_position_control_csv_asset("Adjustments", config=adjustments_config)
-
-stipends_config = {
-    "columns": {
-        "Stipend_ID": "str",
-        "Employee_ID": "str",
-        "Employee_Name": "str",
-        "Stipend_Start_Date": "datetime64[ns]",
-        "Stipend_End_Date": "datetime64[ns]",
-        "Stipend_Status": "str",
-        "Stipend_Category": "str",
-        "Stipend_Description": "str",
-        "Stipend_Amount": "currency",
-        "Stipend_FullAccount": "str",
-        "Stipend_Account": "str",
-        "Stipend_FullGoal": "str",
-        "Stipend_Goal": "str",
-        "Stipend_Location": "str",
-        "Stipend_FullResource": "str",
-        "Stipend_Resource": "str",
-        "Notes": "str",
-    },
-    "non_null": ["Stipend_ID", "Employee_ID", "Employee_Name"],
-    "primary_key": "Stipend_ID",
-    "foreign_keys": {
-        "Employee_ID": "position_control_employees.Employee_ID"
-    }
-}
-position_control_stipends = create_position_control_csv_asset("Stipends", config=stipends_config)
-
-assignments_config = {
-    "columns": {
-        "Assignment_ID": "str",
-        "Assignment_Start": "datetime64[ns]",
-        "Assignment_End": "datetime64[ns]",
-        "Assignment_Count": "int",
-        "Assignment_Status": "str",
-        "Position_ID": "str",
-        "Position_Full": "str",
-        "Employee_ID": "str",
-        "Employee_Count": "int",
-        "Employee_Full": "str",
-        "Assignment_Full": "str",
-        "Assignment_FTE": "float",
-        "Assignment_Calendar": "int",
-        "Assignment_Scale": "str",
-        "Assignment_Step": "int",
-        "Employee_YearsOfServiceStart": "str",
-        "Assignment_Column": "str",
-        "Assignment_Salary": "currency",
-        "Assignment_Wage": "currency",
-        "Assignment_PPP": "currency",
-        "Position_Account": "str",
-        "Position_Goal": "str",
-        "Assignment_Resource": "str",
-        "Assignment_FullResource": "str",
-        "Notes": "str",
-    },
-    "primary_key": "Assignment_ID",
-    "non_null": ["Assignment_ID"],
-    "foreign_keys": {
-        "Position_ID": "position_control_positions.Position_ID",
-        "Employee_ID": "position_control_employees.Employee_ID"
-    }
-}
-position_control_assignments = create_position_control_csv_asset("Assignments", config=assignments_config)
+position_control_positions = create_position_control_csv_asset("Positions", config=pc_positions_config)
+position_control_employees = create_position_control_csv_asset("Employees", config=pc_employees_config)
+position_control_adjustments = create_position_control_csv_asset("Adjustments", config=pc_adjustments_config)
+position_control_stipends = create_position_control_csv_asset("Stipends", config=pc_stipends_config)
+position_control_assignments = create_position_control_csv_asset("Assignments", config=pc_assignments_config)
--- people_team_data/utils/__init__.py
+++ people_team_data/utils/__init__.py
@@ -2,6 +2,8 @@
 import re
 from numbers import Number
 
+import pandas as pd
+
 from .constants import *
 
 
@@ -25,3 +27,24 @@ def currency_to_decimal(s: str):
         return None
     else:
         return re.sub(r"[^\d.]", "", str(s)).strip() or None
+    
+def apply_config_to_dataframe(df: pd.DataFrame, config: dict) -> pd.DataFrame:
+    """Mutatively pplies the configuration to the DataFrame."""
+    # Convert columns to the specified types
+    for column, dtype in config.get("columns", {}).items():
+        if dtype == "currency":
+            df[column] = pd.to_numeric(df[column].apply(currency_to_decimal), errors='coerce', downcast='float')
+        elif dtype == "datetime64[ns]":
+            df[column] = pd.to_datetime(df[column], errors='coerce')
+        elif dtype == "int":
+            df[column] = pd.to_numeric(df[column], errors='coerce', downcast='integer')
+        elif dtype == "float":
+            df[column] = pd.to_numeric(df[column], errors='coerce', downcast='float')
+        else:
+            df[column] = df[column].astype(dtype, errors='ignore')
+
+    # Drop rows with null values in non-null columns
+    non_null_columns = config.get("non_null", [])
+    df.dropna(subset=non_null_columns, inplace=True)
+
+    return df
