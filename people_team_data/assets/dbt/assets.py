f5d357d7433dd2e09736c507e450b60b9e10e2dd
Jack Stehn <jack@caliberschools.org>
2025-05-15 19:51:34 +0000

    Change to use simpler project.py

--- people_team_data/assets/dbt/assets.py
+++ people_team_data/assets/dbt/assets.py
@@ -1,9 +1,9 @@
 from dagster import AssetExecutionContext
 from dagster_dbt import DbtCliResource, dbt_assets
 
-from .project import dbt_project_instance
+from .project import dbt_project
 
 
-@dbt_assets(manifest=dbt_project_instance.manifest_path)
+@dbt_assets(manifest=dbt_project.manifest_path)
 def dbt_models_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
--- people_team_data/assets/dbt/project.py
+++ people_team_data/assets/dbt/project.py
@@ -1,18 +1,11 @@
 from pathlib import Path
 
-from dagster import EnvVar
 from dagster_dbt import DbtProject
 
-DBT_PROJECT_DIR = Path(
-    EnvVar("DBT_PROJECT_DIR").get_value("people_team_data/assets/dbt")
-).resolve()
-DBT_PROFILES_DIR = Path(
-    EnvVar("DBT_PROFILES_DIR").get_value("people_team_data/assets/dbt/.dbt")
-).resolve()
-
-dbt_models_project = DbtProject(
-    project_dir=DBT_PROJECT_DIR,
-    profiles_dir=DBT_PROFILES_DIR,
-    packaged_project_dir=DBT_PROJECT_DIR.joinpath("dbt-project/"),
+dbt_project = DbtProject(
+    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
+    packaged_project_dir=Path(__file__)
+    .joinpath("..", "..", "dbt-project")
+    .resolve(),
 )
-dbt_models_project.prepare_if_dev()
+dbt_project.prepare_if_dev()
