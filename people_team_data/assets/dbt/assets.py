29b1e754280fd048c33caeed995c19c75f7255f4
Jack Stehn <jack@caliberschools.org>
2025-05-15 19:09:59 +0000

    Change dbt structure and add to workflow

--- people_team_data/assets/dbt/assets.py
+++ people_team_data/assets/dbt/assets.py
@@ -1,10 +1,9 @@
 from dagster import AssetExecutionContext
 from dagster_dbt import DbtCliResource, dbt_assets
 
-from .project import dbt_models_project
+from .project import dbt_project_instance
 
 
-@dbt_assets(manifest=dbt_models_project.manifest_path)
+@dbt_assets(manifest=dbt_project_instance.manifest_path)
 def dbt_models_dbt_assets(context: AssetExecutionContext, dbt: DbtCliResource):
     yield from dbt.cli(["build"], context=context).stream()
-    
