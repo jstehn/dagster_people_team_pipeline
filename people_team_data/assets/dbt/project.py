ec5cac96c7d9a81a65d541096bddd8dbd908daf6
Jack Stehn <jack@caliberschools.org>
2025-05-15 19:56:06 +0000

    Remove extra directory pathing

--- people_team_data/assets/dbt/project.py
+++ people_team_data/assets/dbt/project.py
@@ -3,10 +3,8 @@ from pathlib import Path
 from dagster_dbt import DbtProject
 
 dbt_project = DbtProject(
-    project_dir=Path(__file__).joinpath("..", "..", "..").resolve(),
-    profiles_dir=Path(__file__).joinpath("..", "..", "..", ".dbt").resolve(),
-    packaged_project_dir=Path(__file__)
-    .joinpath("..", "..", "dbt-project")
-    .resolve(),
+    project_dir=Path(__file__).joinpath("..").resolve(),
+    profiles_dir=Path(__file__).joinpath("..", ".dbt").resolve(),
+    packaged_project_dir=Path(__file__).joinpath("..", "dbt-project").resolve(),
 )
 dbt_project.prepare_if_dev()
